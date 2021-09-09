aaa437dac6497ac267d081f40c922d24cf52f954
kkwondev <kkwoncokr@gmail.com>
2021-09-09 21:13:25 +0900

    [Working] table 구조

--- src/server.js
+++ src/server.js
@@ -8,8 +8,10 @@ const server = http.createServer(app);
 const PORT = 3000;
 const io = socketio.listen(server);
 
+const {sequelize} = require('../models');
+
 app.get('/', (req,res) => {
-    fs.readFile('./src/chat.html', (error,data) => {
+    fs.readFile('./src/public/chat.html', (error,data) => {
         if (error) {
             console.log(error);
             res.status(500).json({message:"fail"})
@@ -19,15 +21,19 @@ app.get('/', (req,res) => {
     });
     })
 
-// app.get('/', (req,res) => {
-//     res.send('hello world');
-// })
 
     io.sockets.on('connection', socket => {
         socket.on('message', data => {
             io.sockets.emit('message',data);
         })
     })
+
+
+sequelize.sync({force:true}).then(() => {
+    console.log("데이터 베이스 연결 완료.")
+}).catch((e) => {
+    console.error(e);
+})
 
 server.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
