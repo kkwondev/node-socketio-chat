a20d3176dbbe50e1e555e4762a1085fd2d2df34f
kkwondev <kkwoncokr@gmail.com>
2021-09-10 14:13:27 +0900

    [ADD] user signup

--- src/server.js
+++ src/server.js
@@ -6,30 +6,35 @@ const fs = require('fs');
 const app = express();
 const server = http.createServer(app);
 const PORT = 3000;
-const io = socketio.listen(server);
+// const io = socketio.listen(server);
+const authRouter = require('./routes/auth');
+const {sequelize} = require('./models');
 
-const {sequelize} = require('../models');
+app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req,res) => {
-    fs.readFile('./src/public/chat.html', (error,data) => {
-        if (error) {
-            console.log(error);
-            res.status(500).json({message:"fail"})
-        }
-        res.writeHead(200, {'Content-Type': "text/html"});
-        res.end(data);
-    });
-    })
+// app.get('/', (req,res) => {
+//     fs.readFile('./src/public/chat.html', (error,data) => {
+//         if (error) {
+//             console.log(error);
+//             res.status(500).json({message:"fail"})
+//         }
+//         res.writeHead(200, {'Content-Type': "text/html"});
+//         res.end(data);
+//     });
+//     })
+//
+//
+//     io.sockets.on('connection', socket => {
+//         socket.on('message', data => {
+//             io.sockets.emit('message',data);
+//         })
+//     })
+
+app.use('/auth',authRouter);
 
 
-    io.sockets.on('connection', socket => {
-        socket.on('message', data => {
-            io.sockets.emit('message',data);
-        })
-    })
-
-
-sequelize.sync({force:true}).then(() => {
+sequelize.sync().then(() => {
     console.log("데이터 베이스 연결 완료.")
 }).catch((e) => {
     console.error(e);
